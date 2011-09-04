4b7e68fe3d23f1562775a63cb915bab0c75b009b
dal <kedals0@gmail.com>
2011-09-04 10:44:05 +0200

    Scripts added and fixed

--- script/pimpc_replace.sh
+++ script/pimpc_replace.sh
@@ -3,11 +3,13 @@
 # Script used in Nautilus. To associate this script to mimetypes, modify file '~/.local/share/applications/mimeapps.list'
 if [ $# -ne 1 ]
 then 
-    echo "Usage: "$0" <filename>"
+    echo "Usage: "$0" <filename|dir>"
     exit 1
 fi
 max_file=500
-echo $1 | xargs -I{} find "{}" \
+
+mpc clear
+find "${1}" \
     -iname "*.flac" -o -iname "*.mp3" -o -iname "*.ogg" \
     | head -n $max_file | sort \
     | mpc add
