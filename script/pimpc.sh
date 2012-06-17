2bc026b47d9e085b78792bd65db9ef9a919958cf
dal <kedals0@gmail.com>
2012-06-17 11:57:32 +0200

    correct nautilus file association

--- script/pimpc.sh
+++ script/pimpc.sh
@@ -9,22 +9,16 @@ then
 fi
 max_file=500
 
-add()
-{
-    find "${2}" \
-	-iname "*.flac" -o -iname "*.mp3" -o -iname "*.ogg" \
-	| head -n $max_file | sort \
-	| mpc -p ${1} add
-}
+
 
 
 case $2 in
     "add")
-	add $1 "$3"
+	mpc -p ${1} add "$3"
 	;;
     "replace")
 	mpc clear
-	add $1 "$3"
+	mpc -p ${1} add "$3"
 	;;
     *)
 	echo $usage
